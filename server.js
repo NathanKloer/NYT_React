0b20a08ed3b32f189983ed3861b9df0fc2cd6150
Nathan <nathan.kloer@gmail.com>
2019-02-14 13:58:57 -0500

    heroku push

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false, limit: "5mb" }));
 app.use(express.json());
 app.use(express.static("public"));
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
 
 // Handlebars
 app.engine(
